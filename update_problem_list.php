24e0358c1920b4fbc79837dc643b69c844bb48df
Víctor J. Fernández <victor.fernandezgil@educa.madrid.org>
2019-01-07 01:52:17 +0100

    Update update_problem_list.php

--- update_problem_list.php
+++ update_problem_list.php
@@ -3,7 +3,7 @@ $base = dirname(__FILE__); // now $base contains "app"
 include $base.'/db.php';
 
 $myUriBase =  ( (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']);
-$url =str_replace("\\", "/", $myUriBase."jsonuser.php?id=");
+$url =str_replace("\\", "", $myUriBase."/jsonuser.php?id=");
 
 $sql ="select id from usuarios";
 $result = $conn -> query($sql);
